b37461ae19d86577257bd2430c058ce8514699be
TheEnderCore <theendercore@gmail.com>
2025-12-18 20:24:07 +0200

    target_dummy cant be life stolen from anymore

--- src/main/kotlin/com/theendercore/united_armory/data/gen/prov/tags/EntityTypeTagsProvider.kt
+++ src/main/kotlin/com/theendercore/united_armory/data/gen/prov/tags/EntityTypeTagsProvider.kt
@@ -4,6 +4,7 @@ import com.theendercore.united_armory.data.tag.UAEntityTypeTags
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
 import net.minecraft.core.HolderLookup
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.EntityType
 import java.util.concurrent.CompletableFuture
 
@@ -12,5 +13,6 @@ class EntityTypeTagsProvider(o: FabricDataOutput, r: CompletableFuture<HolderLoo
     override fun addTags(arg: HolderLookup.Provider) {
         getOrCreateTagBuilder(UAEntityTypeTags.CANT_BLOODSTEAL)
             .add(EntityType.ARMOR_STAND)
+            .addOptional(ResourceLocation.parse("dummmmmmy:target_dummy"))
     }
 }
