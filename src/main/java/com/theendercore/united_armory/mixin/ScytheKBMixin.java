e93f0f24b1f73e3bd92314f6c8df4a7b5c1c7e73
TheEnderCore <theendercore@gmail.com>
2025-12-19 14:27:03 +0200

    Scythe Crit KB

---
+++ src/main/java/com/theendercore/united_armory/mixin/ScytheKBMixin.java
@@ -0,0 +1,32 @@
+package com.theendercore.united_armory.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.theendercore.united_armory.item.UnnamedScythe;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import static com.theendercore.united_armory.util.MixinKt.doReverseKnockback;
+
+@Mixin(Player.class)
+public abstract class ScytheKBMixin extends LivingEntity {
+    protected ScytheKBMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
+
+    @ModifyExpressionValue(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getKnockback(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/damagesource/DamageSource;)F"))
+    float scytheCustomKB(float original, Entity entity, @Local(ordinal = 2) boolean crit) {
+        var stack = getWeaponItem();
+        if (crit && stack.getItem() instanceof UnnamedScythe) {
+            doReverseKnockback((Player) (Object) this, original, entity);
+            return -100;
+        }
+        return original;
+    }
+}
--- src/main/kotlin/com/theendercore/united_armory/util/mixin.kt
+++ src/main/kotlin/com/theendercore/united_armory/util/mixin.kt
@@ -1,9 +1,55 @@
 package com.theendercore.united_armory.util
 
 import com.theendercore.united_armory.mixin.AttackTickerAccessor
+import net.minecraft.util.Mth
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.ai.attributes.Attributes
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.phys.Vec3
+import kotlin.math.min
 
 fun LivingEntity.attackTicker(): Int = (this as AttackTickerAccessor).un_getAttackStrengthTicker()
 fun LivingEntity.setAttackTicker(value: Int) = (this as AttackTickerAccessor).un_setAttackStrengthTicker(value)
 fun LivingEntity.addAttackTicker(value: Int) = setAttackTicker(attackTicker() + value)
 
+
+fun doReverseKnockback(player: Player, original: Float, entity: Entity) {
+    val kb = -(original + 1f)
+    if (entity is LivingEntity) {
+        entity.cKnockback(
+            kb * 0.5,
+            Mth.sin((player.yRot * (Math.PI / 180.0)).toFloat()).toDouble(),
+            (-Mth.cos((player.yRot * (Math.PI / 180.0)).toFloat())).toDouble()
+        )
+    } else {
+        entity.push(
+            (-Mth.sin(player.yRot * (Math.PI / 180f).toFloat()) * kb * 0.5f).toDouble(),
+            0.1,
+            (Mth.cos(player.yRot * (Math.PI / 180.0).toFloat()) * kb * 0.5f).toDouble()
+        )
+    }
+    player.deltaMovement = player.deltaMovement.multiply(0.6, 1.0, 0.6)
+    player.setSprinting(false)
+}
+
+fun LivingEntity.cKnockback(d: Double, e: Double, f: Double) {
+    var d = d
+    var e = e
+    var f = f
+    d *= 1.0 - getAttributeValue(Attributes.KNOCKBACK_RESISTANCE)
+    this.hasImpulse = true
+    val vec3 = deltaMovement
+
+    while (e * e + f * f < 1.0E-5f) {
+        e = (Math.random() - Math.random()) * 0.01
+        f = (Math.random() - Math.random()) * 0.01
+    }
+
+    val vec32 = Vec3(e, 0.0, f).normalize().scale(d)
+    setDeltaMovement(
+        vec3.x / 2.0 - vec32.x,
+        if (onGround()) min(0.4, vec3.y / 2.0 - d) else vec3.y,
+        vec3.z / 2.0 - vec32.z
+    )
+}
